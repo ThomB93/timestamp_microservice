aab8a4b63d2c56df736b9198151070b73e460b13
ThomB93 <thomb@hotmail.dk>
2020-10-06 22:48:22 +0200

    fix port value

--- server.js
+++ server.js
@@ -45,6 +45,6 @@ app.get("/api/hello", function (req, res) {
 });
 
 // listen for requests :)
-var listener = app.listen(8000, function () {
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
